bcb96253769fafc12adadb94ef97063c1f98db21
navatar <umdomby@yandex.ru>
2021-04-03 01:19:29 +0300

    update

---
+++ backend/src/main/java/org/navatar/multibet/product/ProductDTO.java
@@ -0,0 +1,12 @@
+package org.navatar.multibet.product;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class ProductDTO {
+    private String name;
+    private String description;
+//    private BigDecimal price;
+}
---
+++ backend/src/main/java/org/navatar/multibet/product/ProductRespository.java
@@ -0,0 +1,6 @@
+package org.navatar.multibet.product;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRespository extends JpaRepository<Product, Long> {
+}
